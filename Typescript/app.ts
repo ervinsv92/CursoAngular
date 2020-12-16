c7bd68f9a008fb021d060f81f8b67ab12332aebd
Ervin Solano Vargas <esolanov@grupocolono.com>
2020-12-15 19:47:42 -0600

    desestructuracion

--- Typescript/app.ts
+++ Typescript/app.ts
@@ -6,7 +6,12 @@
         poder:'Aguante'
     }
 
+    //no importa el orden en que se obtengan
     const {nombre, clave, poder} = avenger;
+
+    //console.log(nombre)
+    //console.log(clave)
+    //console.log(poder)
 
     /*Sin desestructuracion
         console.log(avenger.nombre)
@@ -14,7 +19,33 @@
         console.log(avenger.poder)
     */
 
-   console.log(nombre)
-   console.log(clave)
-   console.log(poder)
+    /*sirve
+    const extraer = (avenger:any)=>{
+        const {nombre, clave, poder} = avenger;
+        console.log(nombre)
+        console.log(clave)
+        console.log(poder)
+    }
+    extraer(avenger);
+    */
+
+   const extraer = ({nombre, poder}:any)=>{
+    
+    console.log(nombre)
+    console.log(clave)
+    console.log(poder)
+    }
+//extraer(avenger);
+
+    const avengers:string[] = ['Thor', 'Ironman', 'Spiderman', 'Perro'];
+    //para la destructuracion de array si tiene que ir en orden
+    //para omitir alguno solo se pone la coma
+    const [thor, ironman, , perro] = avengers
+    console.log(thor, ironman, perro)
+
+    const extraerArr = ([thor, ironman, , perro]:string[])=>{
+        console.log(thor, ironman, perro)
+    }
+
+    extraerArr(avengers)
 })();
