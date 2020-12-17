a9c15fd3ff621d8675b86b775c1543b3c73f9b90
Ervin Solano Vargas <esolanov@grupocolono.com>
2020-12-17 15:31:28 -0600

    tipo dato retorno funcion

--- Typescript/app.ts
+++ Typescript/app.ts
@@ -1,4 +1,17 @@
 (()=>{
 
+    const sumar = (a:number, b:number):number => a + b;
+    const nombre = ():string => 'Hola fernando';
 
+    const obtenerSalario = ():Promise<string> => {
+        return new Promise((resolve, reject)=>{
+            resolve("Ervin");
+        });
+    }
+
+    obtenerSalario().then(a => console.log(a.toUpperCase()));
+    /*
+    const sumar = (a:number, b:number):number =>{
+        return a + b;
+    }*/
 })();
