71b173baf5d7cf37e1603b971dcf560ef258ec53
ervinsv92 <ervinsv92@gmail.com>
2020-12-21 13:12:33 -0600

    Hasta clase 44

--- 01-hola-mundo/src/app/app.module.ts
+++ 01-hola-mundo/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import {HeaderComponent} from './components/header/header.component';
+import {BodyComponent} from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
