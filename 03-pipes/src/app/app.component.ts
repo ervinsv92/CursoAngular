906e00ae3395ad43c100ece3424a283aaa13c1d0
Ervin <ervinsv92@gmail.com>
2020-12-29 23:01:03 -0600

    Hasta clase 81

--- 03-pipes/src/app/app.component.ts
+++ 03-pipes/src/app/app.component.ts
@@ -10,4 +10,21 @@ export class AppComponent {
   arreglo = [1,2,3,4,5,6,7,8,9,10];
   PI:number = Math.PI;
   porcentaje:number=0.234;
+  salario:number = 1234.5;
+
+  valorPromesa = new Promise<string>((resolve)=>{
+    setTimeout(() => {
+      resolve('llego la data');
+    }, 4500);
+  });
+
+  heroe = {
+    nombre: 'Logan',
+    clave:'Wolverine',
+    edad:500,
+    direccion:{
+      calle:'Primera',
+      casa:20
+    }
+  };
 }
