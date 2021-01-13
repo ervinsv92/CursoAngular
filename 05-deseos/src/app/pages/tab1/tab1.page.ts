b3342595a548c42d08e25697c737f31eb3bdd86d
ervinsv92 <ervinsv92@gmail.com>
2021-01-12 19:44:36 -0600

    Hasta clase 126

--- 05-deseos/src/app/pages/tab1/tab1.page.ts
+++ 05-deseos/src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { DeseosService } from 'src/app/services/deseos.service';
 
 @Component({
@@ -9,8 +10,42 @@ import { DeseosService } from 'src/app/services/deseos.service';
 })
 export class Tab1Page {
 
-  constructor(public deseosService:DeseosService, private router:Router) {}
-  agregarLista(){
-    this.router.navigateByUrl('/tabs/tab1/agregar');
+  constructor(public deseosService:DeseosService, 
+              private router:Router, 
+              private alertCtrl:AlertController) {}
+
+  async agregarLista(){
+    //this.router.navigateByUrl('/tabs/tab1/agregar');
+    const alert = await this.alertCtrl.create({
+      cssClass: 'my-custom-class',
+      header: 'Nueva lista',
+      inputs:[{
+        name:'titulo',
+        type:'text',
+        placeholder:'Nombre de la lista'
+      }],
+      buttons: [
+        {
+          text:'Cancelar',
+          role:'cancel',
+          handler:()=>{
+            console.log('Cancelar');
+          }
+        },
+        {
+          text:'Crear',
+          handler:(data)=>{
+            console.log(data);
+            if(data.titulo.trim().length == 0) return;
+
+            const id = this.deseosService.crearLista(data.titulo);
+
+            this.router.navigateByUrl(`/tabs/tab1/agregar/${id}`);
+          }
+        }
+    ]
+    });
+
+    await alert.present();
   }
 }
