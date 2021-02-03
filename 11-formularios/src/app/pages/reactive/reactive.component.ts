3b0dfb9673e0cbedb4952552d85943aa321488d9
ervinsv92 <ervinsv92@gmail.com>
2021-02-02 20:21:30 -0600

    Hasta clase 200

--- 11-formularios/src/app/pages/reactive/reactive.component.ts
+++ 11-formularios/src/app/pages/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveComponent implements OnInit {
 
-  constructor() { }
+  forma:FormGroup;
+  constructor(private fb:FormBuilder) { 
+    this.crearFormulario();
+  }
 
   ngOnInit() {
   }
 
+  crearFormulario(){
+    this.forma = this.fb.group({
+      nombre:['', Validators.required],
+      apellido:['', [Validators.required, Validators.minLength(5)]],
+      correo:['', [Validators.required ,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+    });
+  }
+
+  guardar(){
+
+  }
 }
